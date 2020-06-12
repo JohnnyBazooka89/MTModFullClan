baf9beaa78c73c34bedccb531177f0de78838a7b
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2020-06-12 19:45:30 +0200

    v.0.0.1 - Initial Commit

---
+++ FullClanMod/FullClanMod.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+
+namespace FullClanMod
+{
+    [BepInPlugin("FullClanMod", "Full Clan Mod", "0.0.1")]
+    [BepInProcess("MonsterTrain.exe")]
+    [BepInProcess("MtLinkHandler.exe")]
+    public class FullClanMod : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            var harmony = new Harmony("FullClanMod");
+            harmony.PatchAll();
+            Console.WriteLine("FullClanMod 0.0.1");
+        }
+
+    }
+
+    [HarmonyPatch(typeof(ClassSelectionIconUI), "SwapClassIfNecessary")]
+    public class DoNotSwapClassPatch
+    {
+        public static bool Prefix(ClassSelectionUI.ClassOptionData disabledClass, ClassSelectionUI.ClassOptionData replacementClass)
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(SaveManager), "GetRandomUnlockedClass")]
+    public class LetRandomToHitTheSameClassesPatch
+    {
+        public static void Prefix(ref ClassData avoidClass)
+        {
+            avoidClass = null;
+        }
+    }
+}
