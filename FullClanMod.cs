5573eb22c8088c3923471c10fdb1f8b0f66e0774
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2020-10-16 21:57:28 +0200

    Changed project structure and added SteamWorkshopDeployment.

--- FullClanMod.cs
+++ FullClanMod.cs
@@ -1,19 +1,15 @@
-﻿using System;
-using BepInEx;
+﻿using BepInEx;
 using HarmonyLib;
 
 namespace FullClanMod
 {
     [BepInPlugin("FullClanMod", "Full Clan Mod", "0.0.1")]
-    [BepInProcess("MonsterTrain.exe")]
-    [BepInProcess("MtLinkHandler.exe")]
     public class FullClanMod : BaseUnityPlugin
     {
         void Awake()
         {
             var harmony = new Harmony("FullClanMod");
             harmony.PatchAll();
-            Console.WriteLine("FullClanMod 0.0.1");
         }
 
     }
@@ -28,11 +24,12 @@ namespace FullClanMod
     }
 
     [HarmonyPatch(typeof(SaveManager), "GetRandomUnlockedClass")]
-    public class LetRandomToHitTheSameClassesPatch
+    public class LetRandomHitTheSameClassesPatch
     {
         public static void Prefix(ref ClassData avoidClass)
         {
             avoidClass = null;
         }
     }
+
 }
--- README.md
+++ README.md
@@ -4,10 +4,7 @@ A mod that makes it possible to play any clan as primary and secondary at the sa
 
 # Instalation:
 
-1. Install BepInEx (https://github.com/BepInEx/BepInEx/releases) to the same location where your Monster Train is installed (`{your_steam_library_path}\steamapps\common\Monster Train`).
-2. Create folder `plugins` inside BepInEx folder (`{your_steam_library_path}\steamapps\common\Monster Train\BepInEx`).
-3. Download the newest release of FullClanMod.dll file (https://github.com/JohnnyBazooka89/MTModFullClan/releases) and put it into `plugins` folder (`{your_steam_library_path}\steamapps\common\Monster Train\BepInEx\plugins`).
-4. Run the game and the mod should work.
+1. Subscribe to [this mod](https://steamcommunity.com/sharedfiles/filedetails/?id=2259469353) on Monster Train Steam Workshop.
 
 # Screenshots:
 
